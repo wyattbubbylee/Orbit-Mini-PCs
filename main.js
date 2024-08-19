b69ba6edf74ecf8eba3dfdf4275b52ec8067e869
wyatt/gold <119985279+wyattbubbylee@users.noreply.github.com>
2024-08-19 15:51:13 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Orbit Mini PCs website is ready for launch!");
+
+    // Add any JS functionality here (e.g., smooth scrolling, animations)
+});
